d24677e4673142c612754423d75d89949998aa6f
Julianka-GJ <julia.garmarchuk@gmail.com>
2022-06-19 23:49:34 +0300

    expanded functionality

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,24 +1,79 @@
 "use strict";
 
-var btnClickEl = document.getElementById('toggle-btn');
-var enterNameEl = document.getElementById('user-name');
-var listEL = document.getElementById('list-elm');
-listEL.classList.add('name-box-list');
+(function () {
+  var addItemBtnEl = document.getElementById('toggle-btn');
+  var enterNameEl = document.getElementById('user-name');
+  var listEL = document.getElementById('list-elm');
+  var actionBlockEL = document.getElementById('action-block');
+  var checkAllBtnEL = document.getElementById('check-all');
+  var deleteAllBtnEL = document.getElementById('delete-all');
+  listEL.classList.add('name-box-list');
 
-var addElemItem = function addElemItem() {
-  var val = document.getElementById('user-name').value;
-  var newListEL = document.createElement('li');
+  var onAddElemItem = function onAddElemItem() {
+    var val = document.getElementById('user-name').value;
+    var newListEL = document.createElement('li'); // span element
 
-  if (!val.trim()) {
-    alert("Error no name entered!");
-  } else {
-    newListEL.innerHTML = val;
+    var spanEL = document.createElement('span');
+    spanEL.innerHTML = val; // checkbox element
+
+    var newChekEL = document.createElement('input');
+    newChekEL.type = 'checkbox';
+    newChekEL.classList.add('checkbox'); // button element
+
+    var newButEL = document.createElement('button');
+    newButEL.innerHTML = 'Delete';
+    newButEL.classList.add('delete');
+    newChekEL.classList.add('chek-marker');
+    newButEL.classList.add('delet-btn');
+
+    if (!val.trim()) {
+      alert("Error no name entered!");
+      return;
+    }
+
+    if (listEL.childElementCount === 0) {
+      actionBlockEL.classList.remove('hidden');
+    }
+
+    newListEL.append(newChekEL, spanEL, newButEL);
     listEL.append(newListEL);
     enterNameEl.value = '';
-  }
-};
+  };
 
-btnClickEl.addEventListener('click', addElemItem);
-enterNameEl.addEventListener('keydown', function (el) {
-  if (el.code === 'Enter') addElemItem();
-});
+  var onDeletItem = function onDeletItem(el) {
+    if (el.target.classList.contains('delet-btn')) {
+      el.target.parentElement.remove();
+
+      if (listEL.childElementCount === 0) {
+        actionBlockEL.classList.add('hidden');
+      }
+    }
+  };
+
+  var onCheckItem = function onCheckItem(el) {
+    if (el.target.classList.contains('chek-marker')) el.target.parentElement.classList.toggle('checked');
+  };
+
+  var onDeletElemAll = function onDeletElemAll() {
+    Array.from(listEL.children).forEach(function (el) {
+      return el.remove();
+    });
+    actionBlockEL.classList.add('hidden');
+  };
+
+  var onCheckElemAll = function onCheckElemAll() {
+    Array.from(listEL.children).forEach(function (el) {
+      el.classList.add('checked');
+      el.children[0].checked = true;
+    });
+  };
+
+  addItemBtnEl.addEventListener('click', onAddElemItem);
+  enterNameEl.addEventListener('keydown', function (el) {
+    if (el.code === 'Enter') onAddElemItem();
+  });
+  deleteAllBtnEL.addEventListener('click', onDeletElemAll);
+  checkAllBtnEL.addEventListener('click', onCheckElemAll);
+  listEL.addEventListener('click', onDeletItem);
+  listEL.addEventListener('change', onCheckItem);
+})();
--- main.js
+++ main.js
@@ -1,26 +1,88 @@
-const btnClickEl = document.getElementById('toggle-btn');
-const enterNameEl = document.getElementById('user-name');
-const listEL = document.getElementById('list-elm');
 
-listEL.classList.add('name-box-list');
+(() => {
+    const addItemBtnEl = document.getElementById('toggle-btn');
+    const enterNameEl = document.getElementById('user-name');
+    const listEL = document.getElementById('list-elm');
 
+    const actionBlockEL = document.getElementById('action-block');
+    const checkAllBtnEL = document.getElementById('check-all');
+    const deleteAllBtnEL = document.getElementById('delete-all');
 
+    listEL.classList.add('name-box-list');
 
-const addElemItem = () => {
-    let val = document.getElementById('user-name').value;
-    const newListEL = document.createElement('li');
+    const onAddElemItem = () => {
+        let val = document.getElementById('user-name').value;
+        const newListEL = document.createElement('li');
 
-    if (!val.trim()) {
-        alert(`Error no name entered!`);
-    }
-    else {
-        newListEL.innerHTML = val;
+        // span element
+        const spanEL = document.createElement('span');
+        spanEL.innerHTML = val;
+
+        // checkbox element
+        const newChekEL = document.createElement('input');
+        newChekEL.type = 'checkbox';
+        newChekEL.classList.add('checkbox');
+
+        // button element
+        const newButEL = document.createElement('button');
+        newButEL.innerHTML = 'Delete';
+        newButEL.classList.add('delete');
+
+        newChekEL.classList.add('chek-marker');
+        newButEL.classList.add('delet-btn');
+
+        if (!val.trim()) {
+            alert(`Error no name entered!`);
+            return;
+        }
+
+        if (listEL.childElementCount === 0) {
+            actionBlockEL.classList.remove('hidden');
+        }
+        newListEL.append(newChekEL, spanEL, newButEL);
         listEL.append(newListEL);
         enterNameEl.value = '';
-    }
-};
 
-btnClickEl.addEventListener('click', addElemItem);
-enterNameEl.addEventListener('keydown', el => {
-    if (el.code === 'Enter') addElemItem();
-});
+    }
+
+    const onDeletItem = el => {
+        if (el.target.classList.contains('delet-btn')) {
+            el.target.parentElement.remove();
+            if (listEL.childElementCount === 0) {
+                actionBlockEL.classList.add('hidden');
+            }
+        }
+    };
+
+    const onCheckItem = el => {
+        if (el.target.classList.contains('chek-marker'))
+            el.target.parentElement.classList.toggle('checked');
+    };
+
+    const onDeletElemAll = () => {
+        Array.from(listEL.children).forEach(el => el.remove());
+        actionBlockEL.classList.add('hidden');
+    };
+
+    const onCheckElemAll = () => {
+        Array.from(listEL.children).forEach(el => {
+            el.classList.add('checked');
+            el.children[0].checked = true;
+        });
+    };
+
+    addItemBtnEl.addEventListener('click', onAddElemItem);
+    enterNameEl.addEventListener('keydown', el => {
+        if (el.code === 'Enter') onAddElemItem();
+    });
+
+    deleteAllBtnEL.addEventListener('click', onDeletElemAll);
+    checkAllBtnEL.addEventListener('click', onCheckElemAll);
+    listEL.addEventListener('click', onDeletItem);
+    listEL.addEventListener('change', onCheckItem);
+
+})();
+
+
+
+
