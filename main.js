427679d305ffdbd8ba41d6f1fac4ec1148bcb1b7
Julianka-GJ <julia.garmarchuk@gmail.com>
2022-07-12 17:00:44 +0300

    paras were added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
     const loginBtnEl = document.getElementById('button-login');
     const spanEl = document.getElementById('error-message');
 
+
     const listBlockEl = document.getElementById('list-block');
     const addItemBtnEl = document.getElementById('toggle-btn');
     const enterNameEl = document.getElementById('user-name');
@@ -82,42 +83,60 @@
     };
 
 
-//---------------------------------login-form------------------------------------------------
-
-    const user = {
-        login: 'admin@gmail.com',
-        parol: '123'
-    };
-
+    //---------------------------------login-form------------------------------------------------
 
     const entryDataUser = () => {
         let valEmailInput = document.getElementById('email').value;
         let valPasswordInput = document.getElementById('password').value;
 
-        if ((valEmailInput != user.login) || (valPasswordInput != user.parol)) {
-            spanEl.innerHTML = 'You entered incorrect data!';
-            emailinputEl.classList.toggle('error-message');
-            passwordInputEl.classList.toggle('error-message');
+        const user = {
+            login: 'admin@gmail.com',
+            parol: '123'
+        };
 
-            emailinputEl.value = '';
-            passwordInputEl.value = '';
+        //при вводе правильных данных в форму (после нажатия кнопки логина) вместо всей формы должен отобразится TODO List
+        //при вводе неправильных данных - выводим сообщение с информацией о неправильном вводе (форму не прячем, очищаем поле пароля)
+
+        const checkDataUser = () => {
+            if ((valEmailInput != user.login) || (valPasswordInput != user.parol)) {
+                emailinputEl.value = '';
+                passwordInputEl.value = '';
+                spanEl.innerHTML = 'You entered incorrect data!';
+                emailinputEl.classList.toggle('error-message');
+                passwordInputEl.classList.toggle('error-message');
+            }
+
+            else {
+                loginFormEl.classList.toggle('hidden');
+                listBlockEl.classList.remove('hidden');
+            }
+        }
+
+        //кнопка логина должна быть задизейблена если хотя бы один инпут не заполнен
+
+        if ((valEmailInput.length < 1) || (valPasswordInput.length < 1)) {
             loginBtnEl.disabled = true;
+            spanEl.innerHTML = 'Field not filled!';
         }
 
         else {
-            loginFormEl.classList.toggle('hidden');
-            listBlockEl.classList.remove('hidden');
+            checkDataUser();
         }
-    }
+    };
 
-    emailinputEl.onblur = function () {
+    //если в поле логина введено значение отличное от формата email (name@domain.com), при снятии фокуса под инпутом должны показать сообщение о том, что значение введено не по формату
+
+    emailinputEl.onblur = function (evt) {
+        evt.preventDefault();
         if (!emailinputEl.value.includes('@')) {
+            evt.preventDefault();
             emailinputEl.classList.add('invalid');
             spanEl.innerHTML = 'Please, enter correct email!';
         }
     };
 
-    emailinputEl.onfocus = function () {
+    emailinputEl.onfocus = function (e) {
+        loginBtnEl.disabled = false;
         emailinputEl.value = '';
         emailinputEl.classList.remove('error-message');
         spanEl.innerHTML = '';
@@ -127,9 +146,16 @@
     };
 
     passwordInputEl.onfocus = function () {
+        loginBtnEl.disabled = false;
         passwordInputEl.classList.remove('error-message');
         spanEl.innerHTML = '';
     };
+
+
+    loginFormEl.addEventListener('submit', e => {
+        e.preventDefault(),
+            entryDataUser()
+    });
 
 
     addItemBtnEl.addEventListener('click', onAddElemItem);
@@ -141,7 +167,6 @@
     checkAllBtnEL.addEventListener('click', onCheckElemAll);
     listEL.addEventListener('click', onDeletItem);
     listEL.addEventListener('change', onCheckItem);
-    loginBtnEl.addEventListener('click', entryDataUser);
 
 })();
 
